69b85c4a868dbc99af82b845721d5b942b3ef832
viv2711 <vksingh27112001@gmail.com>
2025-07-01 23:43:47 +0530

    complete blog-preview-card frontend-mentor 2nd challenge

---
+++ blog-card/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+return (
+  <main className="container">
+    <div>
+      <img src="./card-img.png" alt="" />
+    <span className="tag">Learning</span>
+    <span className="date">Published 21 Dec 2023</span>
+    <h1 className="title">HTML & CSS foundations</h1>
+    <p className="desc">These languages are the backbone of every website, defining structure, content, and presentation.</p>
+    <div className="profile">
+      <img src="./image-avatar.webp" alt="avatar" />
+      <h2>Greg Hooper</h2>
+    </div>
+    </div>
+  </main>
+)
+}
+
+export default App
