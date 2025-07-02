9fb7e64e6cab1c368115cec616947818375c3fcc
viv2711 <vksingh27112001@gmail.com>
2025-07-02 14:43:58 +0530

    complete frontend mentor 3rd challenge

---
+++ profile-card/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <main className="container">
+      <img className="avatar" src="avatar-jessica.jpeg" alt="avatar" />
+      <h1 className="name">Jessica Randall</h1>
+      <h2 className="location">London, United Kingdom</h2>
+      <p className="desc">"Front-end developer and avid reader."</p>
+      <div className="social-container">
+        <a href="">Github</a>
+      <a href="">Frontend Mentor</a>
+      <a href="">LinkedIn</a>
+      <a href="">Twitter</a>
+      <a href="">Instagram</a>
+      </div>
+    </main>
+  );
+}
+
+export default App;
